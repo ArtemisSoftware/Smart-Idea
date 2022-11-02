976ba2953aaf1b9b71a5bb9877342e0da7913e8c
gustavo <gustavo.isaac@gmail.com>
2022-11-02 09:16:38 +0000

    navigation-compose dependencie

--- app/src/main/java/com/artemissoftware/smartidea/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/smartidea/MainActivity.kt
@@ -10,19 +10,49 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.artemissoftware.smartidea.navigation.Routes
+import com.artemissoftware.smartidea.screens.addEditTodo.AddEditTodoScreen
+import com.artemissoftware.smartidea.screens.todolist.TodoListScreen
 import com.artemissoftware.smartidea.ui.theme.SmartIdeaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SmartIdeaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
                 ) {
-                    Greeting("Android")
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
                 }
             }
         }
