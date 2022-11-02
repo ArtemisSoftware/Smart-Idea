2b73a1145f48c53c1f08300b5bc86ab98ebc19d0
gustavo <gustavo.isaac@gmail.com>
2022-11-02 08:42:47 +0000

    AddEditTodoViewModel

---
+++ app/src/main/java/com/artemissoftware/smartidea/screens/addEditTodo/AddEditTodoEvent.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.smartidea.screens.addEditTodo
+
+sealed class AddEditTodoEvent {
+    data class OnTitleChange(val title: String): AddEditTodoEvent()
+    data class OnDescriptionChange(val description: String): AddEditTodoEvent()
+    object OnSaveTodoClick: AddEditTodoEvent()
+}
